dbc262592978c8f468fc4f5bd207ff5084ef03a4
Turab Ali <turabalibhatti@ymail.com>
2023-09-10 01:03:30 +0500

    Changed href in the NavbarLinkItem (added a '/' before the url)

--- components/navbar/NavbarLinkItem.tsx
+++ components/navbar/NavbarLinkItem.tsx
@@ -12,7 +12,7 @@ export default async function NavbarLinkItem() {
     <ul className='flex justify-between gap-3'>
 
       {categories.map((category:Categories)=>(
-        <Link href={`collections/${category.name}`} key={category._id}>
+        <Link href={`/collections/${category.name}`} key={category._id}>
           <li className='px-5 py-3'>{category.name}</li>
         </Link>
         ))
